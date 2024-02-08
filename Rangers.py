4f9f67968f18087e392b944ef6232f9160420491
JazzyManSerg <jazzymanserg@gmailcom>
2024-02-08 23:41:09 +0200

    test work

--- Rangers.py
+++ Rangers.py
@@ -7,6 +7,7 @@ def run():
     pygame.display.set_caption("Ranger")
     bg_color = (0,0,0)
     gun = Gun(screen)
+    print()
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
