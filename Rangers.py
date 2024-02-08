d258a82a0944993086b716d280d47f65469b9bd0
JazzyManSerg <jazzymanserg@gmail.com>
2024-02-08 22:18:29 +0200

    122

---
+++ Rangers.py
@@ -0,0 +1,17 @@
+import sys
+import pygame
+from Lib.gun import Gun
+def run():
+    pygame.init()
+    screen=pygame.display.set_mode((1200,800))
+    pygame.display.set_caption("Ranger")
+    bg_color = (0,0,0)
+    gun = Gun(screen)
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        screen.fill(bg_color)
+        gun.output()
+        pygame.display.flip()
+run()
